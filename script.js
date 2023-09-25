85613b438e3b1217b365bca9705ae6d14b957738
pmad01 <patrickmadhi@gmail.com>
2023-09-25 16:43:35 +0200

    Copy to clipboard code

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-const copyToClipboard = async function (text) {
-	try {
-		if (navigator.clipboard) {
-			await navigator.clipboard.writeText(text);
-			console.log('Content copied to clipboard');
-		} else {
-			const textArea = document.createElement('textarea');
-			textArea.value = text;
-			textArea.style.position = 'fixed';
-			document.body.appendChild(textArea);
-			textArea.select();
-			document.execCommand('copy');
-			document.body.removeChild(textArea);
-			console.log('Content copied to clipboard (older browser compatible)');
+const copyToClipboard = function (text) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const cleanedText = text.textContent.replace(/\s+/g, ' ').trim()
+			if (navigator.clipboard) {
+				await navigator.clipboard.writeText(cleanedText);
+				resolve();
+			} else {
+				const textArea = document.createElement('textarea');
+				textArea.value = cleanedText;
+				textArea.style.position = 'fixed';
+				document.body.appendChild(textArea);
+				textArea.select();
+				document.execCommand('copy');
+				document.body.removeChild(textArea);
+				resolve();
+			}
+		} catch (err) {
+			reject(err);
 		}
-	} catch (err) {
-		console.error('Failed to copy: ', err);
-	}
+	});
 }
 
 const copyText = document.getElementById('copyText');
 const copyBtn = document.getElementById('copyButton');
-
-copyBtn.addEventListener('click', () => {
-	return copyToClipboard(copyText.innerHTML);
+copyBtn.addEventListener('click', async () => {
+	const copiedText = await copyToClipboard(copyText);
+	return copiedText;
 });
+
